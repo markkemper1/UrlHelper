25bd4b15a7031449268df292ab9b6f52ce8ad89d
Mark Kemper <markkemper1@gmail.com>
2012-07-24 21:28:11 +1000

    Adding default routing files, Updating nuspec.

---
+++ src/UrlHelper/NugetFiles/UrlHelperStartup.cs.pp
@@ -0,0 +1,14 @@
+﻿using System;
+using UrlHelper;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.Routing.UrlHelperStartup), "Start")]
+namespace $rootnamespace$.Routing
+{
+	public static class UrlHelperStartup
+	{
+		public static void Start()
+		{
+			UrlManager<AppUrls>.Initialize();
+		}
+	}
+}
