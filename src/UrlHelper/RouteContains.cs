00390337d86e6c72388b363ad0f099d089d4e9b2
markkemper1 <markkemper1@gmail.com>
2012-10-29 16:09:59 +1100

    bumping version, allowing array to be passed in to route constraint

--- src/UrlHelper/RouteContains.cs
+++ src/UrlHelper/RouteContains.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Routing;
@@ -12,7 +14,12 @@ namespace UrlHelper
 		public RouteContains(params object[] items)
 		{
 			if (items == null) throw new ArgumentNullException("items");
-			this.items = items;
+
+            if (items.Length == 1 && (items[0] is IEnumerable<object>))
+            {
+                items = ((IEnumerable<object>) items[0]).ToArray();
+            }
+		    this.items = items;
 		}
 
 		public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
