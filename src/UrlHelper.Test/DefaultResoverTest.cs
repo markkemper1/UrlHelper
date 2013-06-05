da36fc77e7f57a6937442b1545765d60b0176dbe
markkemper1 <markkemper1@gmail.com>
2013-06-05 19:08:52 +1000

    Adding basic array support for resolve

---
+++ src/UrlHelper.Test/DefaultResoverTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+
+namespace UrlHelper.Test
+{
+    [TestFixture]
+    public class DefaultResoverTest
+    {
+        private DefaultResoverTestAppUrls AppUrls;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ConfigurationManager.AppSettings["BaseUri"] = "http://testing.com";
+            RouteTable.Routes.Clear();
+            AppUrls = UrlManager<DefaultResoverTestAppUrls>.Initialize();
+        }
+
+        [Test]
+        public void Should_be_able_To_resolve_array_of_ints()
+        {
+            Assert.AreEqual(new Uri("http://test.com/tests1/complete?test[0]=1&test[1]=2").PathAndQuery.ToLower(), AppUrls.Tests.Complete(new[] { 1, 2 }).ToString());
+        }
+
+        public class DefaultResoverTestAppUrls : AreaRegistration
+        {
+            public Tests1Urls Tests { get; set; }
+
+            public override void RegisterArea(AreaRegistrationContext routes)
+            {
+                var namespaces = new[] { typeof(UrlManagerTest.SampleController).Namespace };
+
+                routes.MapRoute("Standard", "{controller}/{action}/{id}",
+                                new { controller = "Pages", action = "Index", Area = "public", id = string.Empty },
+                                namespaces
+                    );
+            }
+
+            public override string AreaName
+            {
+                get { return "Public"; }
+            }
+        }
+
+        public class Tests1Urls : UrlsBase
+        {
+            public Uri Complete(int[] ids)
+            {
+                var rvd = Action("Complete");
+                rvd["test"] = ids;
+                return ToUri(rvd);
+            }
+        }
+    }
+}
--- src/UrlHelper/DefaultResolver.cs
+++ src/UrlHelper/DefaultResolver.cs
@@ -1,51 +1,69 @@
 using System;
+using System.Collections;
 using System.Text;
 using System.Web.Routing;
 
 namespace UrlHelper
 {
-	internal class DefaultResolver : IResolver
-	{
-		private readonly RouteCollection routes;
-		private readonly Uri baseUri;
-		private RequestContext requestContext;
-	    public static bool ForceLowerCase = true;
+    internal class DefaultResolver : IResolver
+    {
+        private readonly RouteCollection routes;
+        private readonly Uri baseUri;
+        private RequestContext requestContext;
+        public static bool ForceLowerCase = true;
 
-		public DefaultResolver(RouteCollection routes, Uri baseUri)
-		{
-			if (routes == null) throw new ArgumentNullException("routes");
-			if (baseUri == null) throw new ArgumentNullException("baseUri");
-			this.routes = routes;
-			this.baseUri = baseUri;
-			requestContext = new RequestContext(new RoutingHttpContext(baseUri), new RouteData());
-		}
+        public DefaultResolver(RouteCollection routes, Uri baseUri)
+        {
+            if (routes == null) throw new ArgumentNullException("routes");
+            if (baseUri == null) throw new ArgumentNullException("baseUri");
+            this.routes = routes;
+            this.baseUri = baseUri;
+            requestContext = new RequestContext(new RoutingHttpContext(baseUri), new RouteData());
+        }
 
-		public RouteCollection Routes
-		{
-			get { return routes; }
-		}
+        public RouteCollection Routes
+        {
+            get { return routes; }
+        }
 
-		public Uri Resolve(RouteValueDictionary values)
-		{
-			if (!values.ContainsKey("area"))
-				values["area"] = String.Empty;
+        public Uri Resolve(RouteValueDictionary values)
+        {
+            if (!values.ContainsKey("area"))
+                values["area"] = String.Empty;
 
-			var result = Routes.GetVirtualPath(requestContext, values);
+            var resolveable = new RouteValueDictionary();
+            foreach (var item in values)
+            {
+                if (item.Value is IEnumerable && !(item.Value is string))
+                {
+                    IEnumerable forEachAble = (IEnumerable) item.Value;
+                    int i = 0;
+                    foreach (var x in forEachAble)
+                    {
+                        resolveable.Add(item.Key + "[" + (i++) +"]", x);
+                    }
+                }
+                else
+                    resolveable.Add(item.Key, item.Value);
+            }
 
-			if (result == null)
-			{
-				StringBuilder sb = new StringBuilder();
-				sb.AppendFormat("Could not resolve url. Item Count: {0},  Item Values: ", values.Count);
-				foreach (var item in values)
-				{
-					sb.AppendFormat("{0}={1}, ", item.Key, item.Value);
-				}
-				throw new ArgumentException(sb.ToString());
-			}
+
+            var result = Routes.GetVirtualPath(requestContext, resolveable);
+
+            if (result == null)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("Could not resolve url. Item Count: {0},  Item Values: ", values.Count);
+                foreach (var item in values)
+                {
+                    sb.AppendFormat("{0}={1}, ", item.Key, item.Value);
+                }
+                throw new ArgumentException(sb.ToString());
+            }
 
             var path = ForceLowerCase && result.VirtualPath != null ? result.VirtualPath.ToLowerInvariant() : result.VirtualPath;
 
             return new RouteUri(baseUri, path);
-		}
-	}
+        }
+    }
 }
